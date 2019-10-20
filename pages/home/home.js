6513614ba84264210ea0f45e489632d5585e758f
tangmaoyuan <1028278792@qq.com>
2019-10-20 23:26:28 +0800

    a

--- pages/home/home.js
+++ pages/home/home.js
@@ -2,40 +2,68 @@
 const app = getApp()
 Page({
 
-
-
-
-
   /**
    * 页面的初始数据
    */
   data: {
-    accoutP:666,
-    accoutS:999.00,
-    isSurplus: false,
-    isProp: false,
-    xpur:555,
-    xpro:888,
+    accoutP: 666, //可兑换金额
+    accoutS: 999.00, //可提现金额
+    isSurplus: false, //兑换层
+    isProp: false, //提现层
+    xpur: "", //提现placeholder
+    xpro: "", //兑换placeholder
   },
-/**
+  /**
    * 自定义函数
    */
-  propFun(){//提现开启
+  txWacth(e) {
     this.setData({
-      isProp: true
+      xpur: e.detail.value
     })
-  }, 
-  propFunClose() {//提现关闭
+  },
+  dhWacth(e) {
+    this.setData({
+      xpro: e.detail.value
+    })
+  },
+  tiXianAll() {
+    this.setData({
+      xpur: this.data.accoutS
+    })
+  },
+  duiHuanAll() {
+    this.setData({
+      xpro: this.data.accoutP
+    })
+  },
+  txFun() {
+    wx.navigateTo({
+      url: '../tiXianRecord/tiXianRecord',
+    })
+  },
+  dhFun() {
+    wx.navigateTo({
+      url: '../duiHuanRecord/duiHuanRecord',
+    })
+  },
+  propFun() { //提现开启
+    this.setData({
+      isProp: true,
+      xpur: ""
+    })
+  },
+  propFunClose() { //提现关闭
     this.setData({
       isProp: false
     })
   },
-  proratedFun(){//兑换开启
+  proratedFun() { //兑换开启
     this.setData({
-      isSurplus: true
+      isSurplus: true,
+      xpro: ""
     })
   },
-  proratedFunClose() {//兑换关闭
+  proratedFunClose() { //兑换关闭
     this.setData({
       isSurplus: false
     })
@@ -44,56 +72,56 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-   
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
